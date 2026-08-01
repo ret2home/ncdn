c1fd1f499acc3432c2e700cad2bf23202926c3c0
ret2home <defineprogram@gmail.com>
2026-08-01 19:45:33 +0000

    request collapsing

--- popcache/cache.go
+++ popcache/cache.go
@@ -14,19 +14,21 @@ type Entry struct {
 	header     http.Header
 	data       []byte
 	expire     time.Time
+	finished   bool
+	done       chan struct{}
 }
 type CacheServer struct {
 	origin   *url.URL
-	cachemap map[string]Entry
+	cachemap map[string]*Entry
 	client   *http.Client
 	nodeId   string
-	mu       sync.Mutex
+	mu       sync.RWMutex
 }
 
 func NewCacheServer(origin *url.URL, nodeId string) *CacheServer {
 	cs := CacheServer{
 		origin:   origin,
-		cachemap: make(map[string]Entry),
+		cachemap: make(map[string]*Entry),
 		client: &http.Client{
 			Timeout: 5 * time.Second,
 		},
@@ -34,21 +36,14 @@ func NewCacheServer(origin *url.URL, nodeId string) *CacheServer {
 	}
 	return &cs
 }
-func (c *CacheServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	c.mu.Lock()
-	v, ok := c.cachemap[r.URL.RequestURI()]
-	c.mu.Unlock()
-	if ok && v.expire.After(time.Now()) {
-		for key, values := range v.header {
-			for _, value := range values {
-				w.Header().Add(key, value)
-			}
-		}
-		w.Header().Add("X-Cache", "HIT")
-		w.WriteHeader(v.statusCode)
-		w.Write(v.data)
-		return
-	}
+
+func (c *CacheServer) internalNewRequest(w http.ResponseWriter, r *http.Request) {
+	v := c.cachemap[r.URL.RequestURI()]
+
+	defer func() {
+		v.finished = true
+		close(v.done)
+	}()
 
 	target := c.origin.ResolveReference(r.URL)
 	req, err := http.NewRequest("GET", target.String(), nil)
@@ -78,14 +73,62 @@ func (c *CacheServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	c.mu.Lock()
 	if resp.StatusCode == http.StatusOK {
-		c.mu.Lock()
-		c.cachemap[r.URL.RequestURI()] = Entry{
-			statusCode: resp.StatusCode,
-			header:     resp.Header,
-			data:       buf.Bytes(),
-			expire:     time.Now().Add(time.Second * 60),
-		}
-		c.mu.Unlock()
+		v.statusCode = resp.StatusCode
+		v.header = resp.Header
+		v.data = buf.Bytes()
+		v.expire = time.Now().Add(time.Second * 5)
+		v.finished = true
 	}
+	c.mu.Unlock()
+}
+
+func internalServeCache(v *Entry, w http.ResponseWriter, XCache string) {
+	for key, values := range v.header {
+		for _, value := range values {
+			w.Header().Add(key, value)
+		}
+	}
+	w.Header().Add("X-Cache", XCache)
+	w.WriteHeader(v.statusCode)
+	w.Write(v.data)
+}
+func (c *CacheServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	var useExistCache bool
+	var waitLeader bool
+
+	c.mu.Lock()
+	v, ok := c.cachemap[r.URL.RequestURI()]
+
+	useExistCache = ok && v.finished && v.expire.After(time.Now())
+	waitLeader = ok && !v.finished
+
+	if !useExistCache && !waitLeader {
+		c.cachemap[r.URL.RequestURI()] = &Entry{
+			finished: false,
+			done:     make(chan struct{}),
+		}
+	}
+	c.mu.Unlock()
+
+	if useExistCache {
+		internalServeCache(v, w, "HIT")
+		return
+	}
+
+	if waitLeader {
+		for {
+			<-v.done
+			c.mu.RLock()
+			if v.finished {
+				c.mu.RUnlock()
+				break
+			}
+			c.mu.RUnlock()
+		}
+		internalServeCache(v, w, "COLLAPSED")
+		return
+	}
+	c.internalNewRequest(w, r)
 }
